52746b6d8ce8fa9df74dce3d306a265d68de2990
MechaOphidian <MechaOphidian@gmail.com>
2024-07-05 16:01:51 -0400

    Added Flight
    Plane goes up when you press space
    +Added collision barriers for top & bottom
    +Added Death function to stop animation when plane touches floor

---
+++ Tappy/scenes/plane/plane.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+const GRAVITY: float = 1500.0
+const POWER: float = -500.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	fly(delta)
+	move_and_slide()
+	if is_on_floor() == true:
+		die()
+	
+func fly(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
+
+
+
+func die() -> void:
+	set_physics_process(false)
+	animated_sprite_2d.stop()

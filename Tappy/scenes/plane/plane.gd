6f5f47f6a5c0530a6f3477ad4c76307e0005611f
MechaOphidian <MechaOphidian@gmail.com>
2024-07-06 09:18:30 -0400

    Animation Player
    Plane moves up 20 degrees when pressing space

--- Tappy/scenes/plane/plane.gd
+++ Tappy/scenes/plane/plane.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 
 
 const GRAVITY: float = 1500.0
@@ -23,6 +24,7 @@ func fly(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	if Input.is_action_just_pressed("fly") == true:
 		velocity.y = POWER
+		animation_player.play("power")
 
 
 
